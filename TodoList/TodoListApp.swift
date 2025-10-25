15f75cc74f40310a20ffd4821f18d60ba8efcfdc
Ritesh <riteshvirulkar117@gmail.com>
2025-10-26 01:14:40 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ritesh Virulkar on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
